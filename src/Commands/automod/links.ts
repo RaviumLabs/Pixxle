3c42afc024ddf98eaf48c51a5d949bf88057d262
Striatp <sebastien.works@outlook.com>
2025-03-26 18:22:10 +0100

    Anti-Links

--- src/Commands/automod/links.ts
+++ src/Commands/automod/links.ts
@@ -3,7 +3,24 @@ import configuration from '../../configuration.json';
 
 const Command: IBaseCommand<CommandType> = {
   type: "messageCreate",
-  code: ``,
+  code: `
+$onlyIf[$includes[$getGuildVar[AutoMod_ExcludedChannels;$guildID;];$channelID]==false;]
+
+$arrayLoad[UserRoles;/;$memberRoles[$guildID;$authorID;/]]
+$arrayForEach[UserRoles;Role;
+  $onlyIf[$includes[$getGuildVar[AutoMod_ExcludedRoles;$guildID;];$env[Role]]==false;]
+]
+
+$arrayLoad[BlockedLinks;//SEP//;$getGuildVar[AutoMod_BlockedLinks;$guildID;]]
+$arrayForEach[BlockedLinks;Link;
+  $if[$includes[$message;$env[Link]]==true;$!deleteMessages[$channelID;$messageID]
+    $let[WarninMessageID;$sendMessage[$channelID;
+      $description[$crossmark <@$authorID> The link you have sent is blocked by the Auto-Mod.]
+      $color[${configuration.colors.error}]
+    ;true]]
+    $setTimeout[$!deleteMessages[$channelID;$get[WarninMessageID]];5000]
+  ;]
+]`,
 };
 
 export default Command;
