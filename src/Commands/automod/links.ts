ec589ae79ccd4aa7e85e3ccb42852a9191a0310f
Striatp <sebastien.works@outlook.com>
2025-03-26 16:03:50 +0100

    Changes.

---
+++ src/Commands/automod/links.ts
@@ -0,0 +1,9 @@
+import { CommandType, IBaseCommand } from '@tryforge/forgescript';
+import configuration from '../../configuration.json';
+
+const Command: IBaseCommand<CommandType> = {
+  type: "messageCreate",
+  code: ``,
+};
+
+export default Command;
