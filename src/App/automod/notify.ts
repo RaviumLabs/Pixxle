fbb80f59c3085a15b618bdb6b27e410808944041
Striatp <sebastien.works@outlook.com>
2025-03-30 15:25:27 +0200

    Notification.

--- src/App/automod/notify.ts
+++ src/App/automod/notify.ts
@@ -1,4 +1,5 @@
 import { IApplicationCommandData } from '@tryforge/forgescript';
+import configuration from '../../configuration.json';
 
 const ApplicationCommand: IApplicationCommandData = {
   data: {
@@ -18,7 +19,50 @@ const ApplicationCommand: IApplicationCommandData = {
       }
     ]
   },
-  code: ``,
+  code: `
+$onlyIf[$hasAnyPerms[$guildID;$authorID;Administrator;ManageGuild]==true;$interactionReply[
+  $ephemeral
+  $description[$crossmark You are missing the \`ManageGuild\` permission to run this command.]
+  $color[${configuration.colors.error}]
+]]
+
+$ifx[
+  $if[$option[status]==on;
+    $if[$getGuildVar[AutoMod_Notify;$guildID;false]==false;
+      $setGuildVar[AutoMod_Notify;true;$guildID]
+
+      $interactionReply[
+        $ephemeral
+        $description[$checkmark The Auto-Mod system will now notify users after taking actions.]
+        $color[${configuration.colors.success}]
+      ]
+    ;
+      $interactionReply[
+        $ephemeral
+        $description[$crossmark The Auto-Mod system is already notifying users after taking actions.]
+        $color[${configuration.colors.error}]
+      ]
+    ]
+  ]
+
+  $elseIf[$option[status]==off;
+    $if[$getGuildVar[AutoMod_Notify;$guildID;false]==true;
+      $setGuildVar[AutoMod_Notify;false;$guildID]
+
+      $interactionReply[
+        $ephemeral
+        $description[$checkmark The Auto-Mod system will no longer notify users after taking actions.]
+        $color[${configuration.colors.success}]
+      ]
+    ;
+      $interactionReply[
+        $ephemeral
+        $description[$crossmark The Auto-Mod system is not notifying users after taking actions by default.]
+        $color[${configuration.colors.error}]
+      ]
+    ]
+  ]
+]`,
 };
 
 export default ApplicationCommand;
