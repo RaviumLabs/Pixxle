da3285b5fbf87f43bea0769a3633557646c22caf
Striatp <sebastien.works@outlook.com>
2025-04-03 11:02:34 +0200

    Updated guild API.

--- dist/Routes/api/guilds.js
+++ dist/Routes/api/guilds.js
@@ -15,7 +15,7 @@ module.exports = {
     auth: true,
     handler: function (ctx) {
         return __awaiter(this, void 0, void 0, function* () {
-            const guildList = ctx.client.guilds.cache
+            const sortedGuildList = ctx.client.guilds.cache
                 .sort((a, b) => b.memberCount - a.memberCount)
                 .map(guild => ({
                 name: guild.name,
@@ -23,11 +23,16 @@ module.exports = {
                 owner: guild.ownerId,
                 guildicon: guild.iconURL({ dynamic: true }) || null
             }));
-            const botPing = ctx.client.uptime;
+            const guildList = ctx.client.guilds.cache.map(guild => ({
+                name: guild.name,
+                users: guild.memberCount,
+                owner: guild.ownerId,
+                guildicon: guild.iconURL({ dynamic: true }) || null
+            }));
             ctx.res.send({
                 guildCount: ctx.client.guilds.cache.size,
                 guilds: guildList,
-                ping: botPing
+                sortedGuilds: sortedGuildList
             });
         });
     },
--- src/Routes/Api/guilds.ts
+++ src/Routes/Api/guilds.ts
@@ -5,7 +5,7 @@ module.exports = {
     method: "GET",
     auth: true,
     handler: async function (ctx) {
-        const guildList = ctx.client.guilds.cache
+        const sortedGuildList = ctx.client.guilds.cache
             .sort((a, b) => b.memberCount - a.memberCount)
             .map(guild => ({
                 name: guild.name,
@@ -13,13 +13,19 @@ module.exports = {
                 owner: guild.ownerId,
                 guildicon: guild.iconURL({ dynamic: true }) || null
             }));
-        
-        const botPing = ctx.client.uptime
+
+        const guildList = ctx.client.guilds.cache.map(guild => ({
+            name: guild.name,
+            users: guild.memberCount,
+            owner: guild.ownerId,
+            guildicon: guild.iconURL({ dynamic: true }) || null
+        }));
 
         ctx.res.send({
             guildCount: ctx.client.guilds.cache.size,
             guilds: guildList,
-            ping: botPing
+            sortedGuilds: sortedGuildList
+
         });
     },
 };
